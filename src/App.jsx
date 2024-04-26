ba210911699e47c21c48d3dfa55e4a0d991e9ed5
Larry Osakwe <larryosak@gmail.com>
2024-04-25 19:58:11 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      Hero
+    </section>
+    <section className="padding">
+      Popular product
+    </section>
+    <section className="padding">
+      Super quality
+    </section>
+    <section className="padding-x py-10">
+      Services
+    </section>
+    <section className="padding">
+      SpecialOffer
+    </section>
+    <section className="bg-pale-blue padding">
+      CustomerReviews
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      Subscribe
+    </section>
+    <section className="padding bg-black padding-x padding-t pb-8">
+      Footer
+    </section>
+  </main>
+);
+
+export default App;
